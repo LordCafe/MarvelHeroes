7332cd33af0c0511b65bc1a54f09db607c242bae
Adrian <adrianlopezdelacruz@outlook.com>
2022-03-05 11:39:50 -0600

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+require('dotenv').config();
+
+module.exports = {
+  watch : true,
+  devtool: 'source-map',
+  entry: path.join(__dirname, "src", "index.js"),
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+    new webpack.DefinePlugin({
+      PRODUCTION: JSON.stringify(true),
+      BUILDTIME: JSON.stringify(new Date().getTime()),
+      APIURL: JSON.stringify( process.env.APIURL ),
+      ALLSITES: JSON.stringify(process.env.ALLSITES),
+    }),
+  ],
+}
